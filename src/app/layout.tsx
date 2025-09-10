dc04da40e2851db67ec17d62a926695b14fb5075
Servidor <corondashop@gmail.com>
2025-09-10 10:02:43 +0000

    puedes modificar el icono que aparece en la página web por el que hicimo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/icon-512x512.png" type="image/png" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👁️</text></svg>" />
       </head>
-      <body className="font-body antialiased">
+      <body>
         {children}
         <Toaster />
       </body>
