8d60206b9144b863ff02cbbe5c3e9243ffce40ad
Servidor <corondashop@gmail.com>
2025-09-10 08:13:19 +0000

    revisa que la pagina se adapte correctamente a los móviles

--- src/app/(app)/data-entry/production-form.tsx
+++ src/app/(app)/data-entry/production-form.tsx
@@ -205,7 +205,7 @@ export function ProductionForm() {
                     </FormItem>
                   )}
                 />
-              <div className="grid md:grid-cols-2 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <FormField
                   control={form.control}
                   name="kilosPerBatch"
