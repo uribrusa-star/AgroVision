161b034c1e7f27b099614ef9e10d78a0b9cacd96
Servidor <corondashop@gmail.com>
2025-09-11 10:12:02 +0000

    Try fixing this error: `Runtime Error: Error: Map container is already i

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -6,7 +6,8 @@ import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import 'leaflet-defaulticon-compatibility';
 
 import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
-import { LatLngExpression } from 'leaflet';
+import { LatLngExpression, Map } from 'leaflet';
+import { useRef, useEffect } from 'react';
 
 type MapProps = {
     center: LatLngExpression;
@@ -14,9 +15,18 @@ type MapProps = {
 };
 
 const MapComponent = ({ center, geoJsonData }: MapProps) => {
+    const mapRef = useRef<Map | null>(null);
+
+    useEffect(() => {
+        return () => {
+            if (mapRef.current) {
+                mapRef.current.remove();
+            }
+        };
+    }, []);
 
     return (
-        <MapContainer center={center} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
+        <MapContainer whenCreated={map => mapRef.current = map} center={center} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
