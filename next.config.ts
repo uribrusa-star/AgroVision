68e6ac9ac503b640d3f06a179f5cbef60a973eae
Servidor <corondashop@gmail.com>
2025-09-15 03:12:12 +0000

    tengo este error: Fallo al cargar 'https://firestore.googleapis.com/goog

--- next.config.ts
+++ next.config.ts
@@ -11,13 +11,28 @@ const withPWA = require('@ducanh2912/next-pwa').default({
       handler: 'NetworkOnly',
     },
     {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif|ico|webp)$/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images-cache',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    },
+    {
       urlPattern: /^https?.*/i,
       handler: 'NetworkFirst',
       options: {
-        cacheName: 'pages-cache',
+        cacheName: 'other-requests-cache',
+        networkTimeoutSeconds: 10,
         expiration: {
           maxEntries: 50,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+          maxAgeSeconds: 10 * 24 * 60 * 60, // 10 days
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
         },
       },
     },
