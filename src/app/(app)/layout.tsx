f0eaeb7ff3ae85a2d4ed1194207c40dd9069e80e
Servidor <corondashop@gmail.com>
2025-09-30 18:25:38 +0000

    modifica la class de "flex min-h-screen" para que ocupe el 100% de la pa

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -302,7 +302,7 @@ function AppLayoutContent({ children }: { children: React.ReactNode }) {
 
   return (
       <SidebarProvider>
-        <div className="flex min-h-screen">
+        <div className="flex h-screen">
           <Sidebar collapsible='offcanvas'>
             <SidebarHeader className="p-4">
               <div className="flex items-center gap-2">
